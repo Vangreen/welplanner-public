d27562e382b2e3de2099fe109e97a0736b44df8e
vangreen <pwangryn@op.pl>
2019-11-26 17:48:55 +0100

    Add bottom navigation bar with date picker timeline

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,10 +4,6 @@ import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:wel_planner/model/event.dart';
-import 'package:wel_planner/api/event_api.dart';
-
-
-import 'lesson_info.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
@@ -20,8 +16,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   DateTime _selectedValue = DateTime.now();
-
-
 
   List<Event> _events = List<Event>();
 
@@ -50,64 +44,48 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      _events[index].lessonName,
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      _events[index].location,
-                      style: TextStyle(color: Colors.grey.shade600),
-                    ),
-                  ],
-                ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    _events[index].lessonName,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    _events[index].location,
+                    style: TextStyle(color: Colors.grey.shade600),
+                  ),
+                ],
               ),
-            );
+            ),
+          );
+        },
+        itemCount: _events.length,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        //  padding: EdgeInsets.all(20.0),
+        child: DatePickerTimeline(
+          _selectedValue,
+          onDateChange: (date) {
+            // New date selected
+            setState(() {
+              _selectedValue = date;
+            });
           },
-          itemCount: _events.length,
-//        body: Container(
-//          padding: EdgeInsets.all(20.0),
-//          child: Column(
-//            crossAxisAlignment: CrossAxisAlignment.center,
-//            mainAxisSize: MainAxisSize.max,
-//            mainAxisAlignment: MainAxisAlignment.end,
-//            children: <Widget>[
-//              Text("You Selected:"),
-//              Padding(
-//                padding: EdgeInsets.all(10),
-//              ),
-//              Text(_selectedValue.toString()),
-//              Padding(
-//                padding: EdgeInsets.all(20),
-//              ),
-//              DatePickerTimeline(
-//                _selectedValue,
-//                onDateChange: (date) {
-//                  // New date selected
-//                  setState(() {
-//                    _selectedValue = date;
-//                  });
-//                },
-//              ),
-//            ],
-//          ),
-//        ));
-        ));
+        ),
+      ),
+    );
   }
 }
