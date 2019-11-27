f6cd616fdaf0ba0c4f189e5a5359b9d456f65228
vangreen <pwangryn@op.pl>
2019-11-27 16:54:19 +0100

    Add drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:convert';
+import 'dart:developer' as developer;
 
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:wel_planner/model/event.dart';
-
-//tbdeleted
-import 'dart:developer' as developer;
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
@@ -21,22 +19,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   DateTime _selectedValue = DateTime.now();
 
+  //To sluzy do dopasowania elementow do wyswietlacza telefonu
+  //dzieki temu wyswietlane elementy beda rowne na kazdym telefonie
+  Size screenSize(BuildContext context) {
+    return MediaQuery.of(context).size;
+  }
 
+  double screenWidth(BuildContext context, {double dividedBy = 1}) {
+    return screenSize(context).width / dividedBy;
+  }
 
   List<Event> _events = List<Event>();
 
   Future<List<Event>> fetchEvent() async {
     String formattedDate = DateFormat('yyyy-MM-dd').format(_selectedValue);
+    //tbdeleted
     developer.log(formattedDate);
-    var url = 'http://championships.ringo.org.pl/event/e6c1s1/$formattedDate.json';
-    developer.log('http://championships.ringo.org.pl/event/e6c1s1/$formattedDate.json');
+    var url =
+        'http://championships.ringo.org.pl/event/e6c1s1/$formattedDate.json';
+    //tbdeleted
+    developer.log(
+        'http://championships.ringo.org.pl/event/e6c1s1/$formattedDate.json');
 
     var response = await http.get(url);
 
     var events = List<Event>();
 
     if (response.statusCode == 200) {
-
       var eventJson = json.decode(response.body);
       for (var noteJson in eventJson) {
         events.add(Event.fromJson(noteJson));
@@ -60,6 +69,21 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      drawer: SizedBox(
+        width: screenSize(context).width / 2.2,
+        child: Drawer(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text("E6C1S1"),
+              ),
+              ListTile(
+                title: Text("E6T1S1"),
+              ),
+            ],
+          ),
+        ),
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
@@ -99,7 +123,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 });
               });
             });
-         //   initState();
+            //   initState();
           },
         ),
       ),
